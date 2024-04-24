3fcc6847a7c9b12db01d20ca6bba5cd3f14390bd
Yokojona <yokojona@gmail.com>
2024-04-24 20:10:40 +0300

    Created Engine Interface

---
+++ Engine/src/EngineImp.java
@@ -0,0 +1,31 @@
+public class EngineImp implements Engine {
+    @Override
+    public void readSystemXML() {
+
+    }
+
+    @Override
+    public void displayGameDetails() {
+
+    }
+
+    @Override
+    public void startNewGame() {
+
+    }
+
+    @Override
+    public void gameTurn() {
+
+    }
+
+    @Override
+    public void gameStatus() {
+
+    }
+
+    @Override
+    public void exitSystem() {
+
+    }
+}

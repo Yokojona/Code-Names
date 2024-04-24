c66bd47b58a2f22c10219e162f5c27cdb8b2dd6c
Yokojona <yokojona@gmail.com>
2024-04-24 20:46:22 +0300

    Used board and Engine in main

--- Engine/src/Engine.java
+++ Engine/src/Engine.java
@@ -1,6 +1,8 @@
 public interface Engine {
     public void readSystemXML();
+
     public void displayGameDetails();
+
     public void startNewGame();
     public void gameTurn();
     public void gameStatus();
--- Engine/src/Main.java
+++ Engine/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Board board = new Board();
         Engine engine = new EngineImp();
+
     }
 }
